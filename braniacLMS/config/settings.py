f11d5aeddd2ea06be68aab81897b8896753934fc
Elena <verdaindi-lena@yandex.ru>
2022-10-30 16:27:43 +0800

    edit contacts.html

--- braniacLMS/config/settings.py
+++ braniacLMS/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'email',
-
-
+    'mainapp',
 
 ]
 
@@ -58,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'mainapp/templates', BASE_DIR/'venv'],
+        'DIRS': [BASE_DIR , 'mainapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
